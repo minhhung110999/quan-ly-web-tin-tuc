0f5709dc464441b864fe3d414ad79b94cb782ef5
minhhung110999 <nminhhung858123@gmail.com>
2020-12-21 09:53:43 +0700

    set up source

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,12 @@
+
+ <!-- Footer -->
+    <hr>
+    <footer>
+        <div class="row">
+            <div class="col-md-12">
+				   <center> Sinh Viên Thực Hiện : Nguyễn Minh Hùng, Đào Đăng Quang, Đỗ Đình Huy</center> <br />
+            </div>
+        </div>
+    </footer>
+    <!-- end Footer -->
+
